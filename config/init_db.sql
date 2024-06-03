c964c7730199ad7abb5c00277b8a14e14115baec
Alexey Korev <alexeykor07@gmail.com>
2024-06-03 10:57:05 +0400

    less13 commit

---
+++ config/init_db.sql
@@ -0,0 +1,13 @@
+create table public.resume (
+    uuid      character(36) primary key not null,
+    full_name text                      not null
+);
+
+create table public.contact (
+    id          serial,
+    resume_uuid character(36) not null references resume (uuid) on delete cascade,
+    type        text          not null,
+    value       text          not null
+);
+create unique index contact_uuid_type_index
+    on contact (resume_uuid, type);

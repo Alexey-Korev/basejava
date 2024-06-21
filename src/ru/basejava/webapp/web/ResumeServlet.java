019ef5a92573ff3a029d189bc11d9610552b3679
Alexey Korev <alexeykor07@gmail.com>
2024-06-21 09:57:04 +0400

    hw15 resumes html table implementation

--- src/ru/basejava/webapp/web/ResumeServlet.java
+++ src/ru/basejava/webapp/web/ResumeServlet.java
@@ -1,21 +1,54 @@
 package ru.basejava.webapp.web;
 
+import ru.basejava.Config;
+import ru.basejava.webapp.model.Resume;
+import ru.basejava.webapp.storage.Storage;
+
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.Writer;
 
 public class ResumeServlet extends HttpServlet {
+    private final Storage storage = Config.get().getStorage();
+
+    static {
+        try {
+            Class.forName("org.postgresql.Driver");
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException("Failed to load PostgreSQL driver", e);
+        }
+    }
+
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws javax.servlet.ServletException, IOException {
 
     }
-
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws javax.servlet.ServletException, IOException {
         request.setCharacterEncoding("UTF-8");
         response.setCharacterEncoding("UTF-8");
 //        response.setHeader("Content-Type", "text/html; charset=UTF-8");
         response.setContentType("text/html; charset=UTF-8");
-        String name = request.getParameter("name");
-        response.getWriter().write(name == null ? "Hello Resumes!" : "Hello " + name + '!');
+//        String name = request.getParameter("name");
+//        response.getWriter().write(name == null ? "Hello Resumes!" : "Hello " + name + '!');
+        Writer writer = response.getWriter();
+        writer.write(
+                "<html>\n" +
+                        "<body>\n" +
+                        "<table border=\"1\" cellpadding=\"5\" cellspacing=\"5\">\n" +
+                        "    <tr>\n" +
+                        "        <th>uuid</th>\n" +
+                        "        <th>full name</th>\n" +
+                        "    </tr>\n");
+        for (Resume resume : storage.getAllSorted()) {
+            writer.write(
+                    "<tr>\n" +
+                            "     <td>" + resume.getUuid() + "</td>\n" +
+                            "     <td>" + resume.getFullName() + "</td>\n" +
+                            "</tr>\n");
+        }
+        writer.write("</table>\n" +
+                    "</body>\n" +
+                    "</html>\n");
     }
 }
